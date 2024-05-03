7731cbd78e0685ba4dfec8e08cec088a0e8ba265
jbreffle <78390184+jbreffle@users.noreply.github.com>
2024-05-03 08:35:16 -0400

    Set up minimal app

---
+++ streamlit/Home.py
@@ -0,0 +1,13 @@
+"""Home page for streamlit app."""
+
+import streamlit as st
+
+
+def main():
+    st.title("arXiv Chat")
+    st.write("Chat with recent arXiv papers!")
+    return
+
+
+if __name__ == "__main__":
+    main()
